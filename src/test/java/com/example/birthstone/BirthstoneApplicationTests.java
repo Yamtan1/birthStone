4373c73c53dfd193b9b31031d7de5064d8c386f2
Yamtan1 <gndndk12305@naver.com>
2025-06-23 12:14:13 +0900

    Initial commit

---
+++ src/test/java/com/example/birthstone/BirthstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.birthstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
